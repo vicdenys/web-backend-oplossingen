bd1fbcf15ec84584a3fde282244aa299bfb588b4
Vic Denys <wernerdenys@VIC-DENYS.local>
2014-10-05 19:51:01 +0200

    Oplossing toegevoegd

---
+++ While/Deel1.php
@@ -0,0 +1,35 @@
+<?php
+	$getallen = array();
+	$getal = 0;
+	while ($getal < 101){
+		$getallen[] = $getal;
+		$getal++;
+	}
+	$reeks = implode(", ", $getallen);
+	
+	$getal = 0;
+	$getallen2 = array();
+	while ($getal <= 80)
+	{
+		if ($getal % 3 == 0 && $getal > 40 && $getal < 80){
+			$getallen2[] = $getal;
+		}
+		$getal ++;
+	}
+	$reeks2 = implode(", ", $getallen2);
+	
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+		<meta charset="utf-8"/>
+		
+	</head>
+	<body>
+		<h1>Oplossing while Deel 1</h1>
+		<p>reeks 1 = <?=$reeks?></p>
+		<p>reeks 2 = <?=$reeks2?></p>
+	</body>
+</html>
---
+++ While/Deel2.php
@@ -0,0 +1,36 @@
+<?php
+	$tafel = 0;
+	$product = 1;
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+		<meta charset="utf-8"/>
+		<style>
+			.odd{
+				background-color: green;
+			}
+		</style>
+		
+	</head>
+	<body>
+		<h1>Oplossing while Deel 2</h1>
+		<h3>Tabel</h3>
+		
+		<table>
+			<?php while($tafel <= 10): ?>
+			<tr>
+				<?php $product = 1?>
+				<?php while ($product <= 10): ?>
+					<td class="<?= ( ( $tafel * $product ) % 2 > 0 ) ? : 'odd' ?>"><?=$product * $tafel?></td>
+					<?php $product++;?>
+				<?php endwhile?>
+			</tr>
+			<?php $tafel ++; ?>
+			<?php endwhile ?>
+		</table>
+		
+	</body>
+</html>
