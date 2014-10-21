aac257c6743ac9c62670b09b97b351a0d989bfd2
Vic Denys <wernerdenys@VIC-DENYS.local>
2014-10-21 10:21:34 +0200

    opdracht include/require

---
+++ Include-require/index.php
@@ -0,0 +1,26 @@
+<?php
+	
+	$artikels = array(
+		array(
+				"title" => "lalalala",
+				"text" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+				"tags" => array("tag1","tag2") 
+		),
+		array(
+				"title" => "lelelele",
+				"text" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+				"tags" => array("tag1") 
+		),
+		array(
+				"title" => "lolololo",
+				"text" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+				"tags" => array("tag1","tag2") 
+		)
+	);
+	
+	include("view/header-partial.html");
+	include("view/body-partial.html");
+	include("view/foot-partial.html");
+	
+	
+?>
