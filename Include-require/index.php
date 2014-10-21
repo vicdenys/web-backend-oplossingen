5471e2b1bf6633681b55bd6226e4d0ffd91f63fe
Vic Denys <wernerdenys@VIC-DENYS.local>
2014-10-21 10:33:56 +0200

    stijlaanpassingen include/require

--- Include-require/index.php
+++ Include-require/index.php
@@ -9,7 +9,7 @@
 		array(
 				"title" => "lelelele",
 				"text" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-				"tags" => array("tag1") 
+				"tags" => array("tag1","tag2") 
 		),
 		array(
 				"title" => "lolololo",
