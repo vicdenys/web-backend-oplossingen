51e012c13bd37d2262671cc6f6900485ed3010c0
Vic Denys <wernerdenys@VIC-DENYS.local>
2014-11-18 13:21:44 +0100

    begin

---
+++ Classes Portofolio/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Portofolio</title>
+		<meta charset="utf-8"/>
+		
+		<link rel="stylesheet" type="style/css" href="CSS/global.css"/>
+	</head>
+	<body>
+		<header>
+			<h3>Vic Denys</h3>
+			<ul>
+				<li>Contact</li>
+				<li>Gallerij</li>
+				<li>Werk</li>
+			</ul>
+		</header>
+		<div id="body">
+			
+		</div>
+		<footer>
+			
+		</footer> 
+
+	</body>
+</html>
